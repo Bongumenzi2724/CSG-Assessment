bb6502888d59dd2bd4c90a99a6a3e25f19109712
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-09-03 06:21:41 +0200

    "Fourth Commit"

--- src/components/InformationCard.tsx
+++ src/components/InformationCard.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
 import ReactAudioPlayer from 'react-audio-player'
 
-interface ItemsProps{
-  items:any[]
-}
+/* interface ItemsProps{
+  items:any[],
+  items.Image:any
+} */
   let mainURL="https://arthurfrost.qflo.co.za/";
 
-const InformationCard: React.FC<ItemsProps> = ({item}:ItemsProps) => {
+const InformationCard = ({items}:any) => {
   
   const imageStr='Images/CommunionThumb.jpg'
   mainURL=mainURL.replace(/^"|"$/g,'');
   //let imageURL=item.Image.replace(/^"|"$/g,'');
-  let imageURL=item.Image!=null?item.Image.replace(/^"|"$/g,''):imageStr.replace(/^"|"$/g,'');
+  let imageURL=items.Image!=null?items.Image.replace(/^"|"$/g,''):imageStr.replace(/^"|"$/g,'');
   imageURL=`${mainURL}/${imageURL}`;
 
   //let audioURL=item.Audio.replace(/^"|"$/g,'');
   
   const audioStr='MP3/XmPqugb4EqQ.mp3'
-  let audioURL=item.Audio!=null?item.Audio.replace(/^"|"$/g,''):audioStr.replace(/^"|"$/g,'');
+  let audioURL=items.Audio!=null?items.Audio.replace(/^"|"$/g,''):audioStr.replace(/^"|"$/g,'');
   audioURL=`${mainURL}/${audioURL}`;
 
   return (
@@ -28,21 +28,21 @@ const InformationCard: React.FC<ItemsProps> = ({item}:ItemsProps) => {
       <div className="py-4 flex flex-col gap-3">
         <div className="flex justify-between items-start flex-col gap-2">
           <h2 className="text-xl font-bold text-[#442d63] line-clamp-1 w-full">
-            Title: {item.Title}
+            Title: {items.Title}
           </h2>
           <div className="py-1 px-2 bg-slate-300 rounded-xl">
             <ReactAudioPlayer controls src={audioURL} className='flex justify-center items-center p-1'/>
           </div>
         </div>
         <div className="flex flex-col justify-center items-start gap-2">
-          <h3 className='text-base font-bold text-[#163d69]'>Category: {item.Category}</h3>
+          <h3 className='text-base font-bold text-[#163d69]'>Category: {items.Category}</h3>
           <p className="text-base text-[#163d69] font-bold">
-            Create Date : {item.CreateDate}
+            Create Date : {items.CreateDate}
           </p>
         </div>
         <div className="flex flex-row gap-10 items-center">
-          <p className="text-base font-bold text-[#326084]">Episode: {item.Episode}</p>
-          <p className="text-base font-bold text-[#326084]">MediaName: {item.MediaName}</p>
+          <p className="text-base font-bold text-[#326084]">Episode: {items.Episode}</p>
+          <p className="text-base font-bold text-[#326084]">MediaName: {items.MediaName}</p>
         </div>
       </div>
     </div>
--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,9 +1,11 @@
-import React, { useState,useEffect } from 'react';
+import  { useState,useEffect } from 'react';
 import InformationCard from './InformationCard';
 
 import axios from 'axios';
 const url="https://arthurfrost.qflo.co.za/php/getTimeline.php";
-
+/* interface itemProps{
+    item:any[]
+} */
 const MainCard = () => {
     //fetch the data from the api
     const [dataItems, setDataItems] = useState<any[]>([]);
@@ -29,7 +31,7 @@ const MainCard = () => {
     <section className='flex flex-col gap-10 px-6 md:px-20 py-24'>
         <div className='flex flex-wrap gap-x-8 gap-y-16'>
            {dataItems?.map((data)=>(
-            <InformationCard key={data.Id} item={data}/>
+            <InformationCard key={data.Id} items={data}/>
            ))} 
         </div>
     </section>
